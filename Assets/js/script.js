df6c7faad6e7355e827b4b6a288170e3759c1a5e
Hemanth Varyani <hevary@microsoft.com>
2022-08-18 16:24:18 -0700

    updated the code to populate 5 day weather forcast

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,7 +3,8 @@ var searchbtn = document.querySelector(".searchbtn");
 var city = "";
 var date = moment();
 
-var currentDate = date.format("D/MM/YYYY");
+
+var currentDate = date.format("MM/D/YYYY");
 console.log(currentDate); // "17/06/2022"
 
 searchbtn.onclick = function (event) {
@@ -36,7 +37,7 @@ function citySearch() {
         document.querySelector(".imgIcon").src =
           "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
         //temp
-        document.querySelector(".temp").innerHTML = "Temp: " + data.main.temp;
+        document.querySelector(".temp").innerHTML = "Temp: " + data.main.temp +"&#8457";
         //wind
         document.querySelector(".wind").innerHTML =
           "Wind: " + data.wind.speed + " MPH";
@@ -51,7 +52,7 @@ function citySearch() {
           "&lon=" +
           data.coord.lon +
           "&exclude=hourly,minutely&appid=" +
-          APIKey;
+          APIKey+"&units=imperial";;
         fetch(forcastquery)
           .then(function (response) {
             return response.json();
@@ -62,18 +63,31 @@ function citySearch() {
             document.querySelector(".uvIndex").innerHTML = data.current.uvi;
 
             var cardbody = document.querySelector(".carddata");
-          
+
             for (var i = 1; i <= 5; i++) {
-              var dateval = new Date((data.daily[i].dt)*1000).toLocaleDateString("en-US");
+              var dateval = new Date(
+                data.daily[i].dt * 1000
+              ).toLocaleDateString("en-US");
               console.log(dateval);
-              cardbody.innerHTML = ` <div class="col">
-          <div class="card h-100">
-            <div class="card-body">
-              <h5 class="card-title">${dateval}</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
+              var temp= data.daily[i].temp.day;
+              var wind= data.daily[i].wind_speed;
+              var humidity=data.daily[i].humidity;
+
+              const element = document.createElement('div');
+
+              element.innerHTML = ` 
+              <div class="col">
+            <div class="card h-100">
+            <div class="vstack gap-3">
+            <h5 class="dateval">${dateval}</h5>
+            <h5><img src="http://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png";"></h5>
+            <h5 class="temp">Temp: ${temp} &#8457</h5>
+            <h5 class="wind">Wind: ${wind} MPH</h5>
+            <h5 class="humidity">Humidity: ${humidity} %</h5>
+           </div>
           </div>
         </div>`;
+        cardbody.appendChild(element);
             }
           });
       });
