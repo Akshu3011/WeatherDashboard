265f5811d10c307baa30071397eee96c0a21e68d
Akshata Kumbhar <105023659+Akshu3011@users.noreply.github.com>
2022-08-17 23:51:14 -0700

    added the UI for searching the city

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,16 @@
-alert("linked");
+
+var APIKey="ddeaf2e65b5db636874978d44d4454d3";
+var city="Seattle"
+//var queryURL = "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={ddeaf2e65b5db636874978d44d4454d3}";
+//var queryURL= "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid="+APIKey;
+var queryURL="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey; 
+//var queryURL="https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid="+APIKey;
+fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+    }).then(function (data) {
+      console.log(data);});
+
+//var responseText = document.getElementById('response-text');
+
+
